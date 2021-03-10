a675ef6498fc25b56630bd8c20fc4a872b7fdc9d
Basimon <74089102+basimonpp@users.noreply.github.com>
2021-03-10 10:54:54 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -4,20 +4,52 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
 @Client.on_message(
     filters.command("start")
+    & filters.private
+    & ~ filters.edited
+)
+async def start_(client: Client, message: Message):
+    await message.reply_text(
+        f"""<b> hola {message.from_user.first_name}!</b>
+
+iam a music bot to play music in your groups voice chat.
+
+contact creator to created my clone or for any questions""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "SUPPORT", url="https://t.me/nimmisupport"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "group", url="https://t.me/unitedbotssupport"
+                    ),
+                    InlineKeyboardButton(
+                        "Channel", url="https://t.me/basi_mon"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+@Client.on_message(
+    filters.command("start")
     & filters.group
     & ~ filters.edited
 )
 async def start(client: Client, message: Message):
     await message.reply_text(
-        "💁🏻‍♂️ Do you want to search for a YouTube video?",
+        "Do you want to search for a YouTube video?",
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "✅ Yes", switch_inline_query_current_chat=""
+                        "Yes", switch_inline_query_current_chat=""
                     ),
                     InlineKeyboardButton(
-                        "No ❌", callback_data="close"
+                        "No", callback_data="close"
                     )
                 ]
             ]
