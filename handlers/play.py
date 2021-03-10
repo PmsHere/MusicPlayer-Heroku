63462d03a283226a273454db3ffcd8ba2d5b7850
Basimon <74089102+basimonpp@users.noreply.github.com>
2021-03-10 12:39:13 +0530

    Update play.py

--- handlers/play.py
+++ handlers/play.py
@@ -19,7 +19,7 @@ from helpers.errors import DurationLimitError
 async def play(client: Client, message_: Message):
     audio = (message_.reply_to_message.audio or message_.reply_to_message.voice) if message_.reply_to_message else None
 
-    res = await message_.reply_text("get it in process powerd by @basi_mon")
+    res = await message_.reply_text("starting...")
 
     if audio:
         if round(audio.duration / 60) > DURATION_LIMIT:
@@ -50,7 +50,7 @@ async def play(client: Client, message_: Message):
                         break
 
         if offset == None:
-            await res.edit_text("❕ You did not give me anything to play.")
+            await res.edit_text("send me a youtube link.")
             return
 
         url = text[offset:offset+length]
@@ -64,7 +64,7 @@ async def play(client: Client, message_: Message):
 
     if is_playing:
         position = await sira.add(message_.chat.id, file_path)
-        await res.edit_text(f"#️⃣ Queued at position {position}.")
+        await res.edit_text(f"Queued at position {position}.")
     else:
         await res.edit_text("playing... backup is on")
         tgcalls.pytgcalls.join_group_call(message_.chat.id, file_path, 48000)
